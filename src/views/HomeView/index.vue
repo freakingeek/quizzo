fe246a86a57723c47522c979b9b795691b00a70f
Yasin Silavi <ys.silavi@gmail.com>
2022-05-21 21:56:41 +0430

    feat: home page header WIP

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>Hello From Home View!</h1>
+  <DefaultLayout>
+    <header class="header">
+      <span class="header__text">آماده یه مسابقه کوچیک هستی؟</span>
+      <RouterLink class="header__link" to="#">شروع مسابقه جدید</RouterLink>
+    </header>
+  </DefaultLayout>
 </template>
 
 <script>
+import DefaultLayout from '/src/layouts/DefaultLayout/index.vue';
+
 export default {
   name: 'HomeView',
+
+  components: {
+    DefaultLayout,
+  },
 };
 </script>
 
