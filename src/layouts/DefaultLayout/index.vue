9718747f8b4189b1d4ce9de38b64b4159925e0ec
Yasin Silavi <ys.silavi@gmail.com>
2022-05-21 21:02:38 +0430

    feat: add default layout

---
+++ src/layouts/DefaultLayout/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="default-layout">hello!</main>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+};
+</script>
+
+<style src="./DefaultLayout.scss" lang="scss" scoped />
