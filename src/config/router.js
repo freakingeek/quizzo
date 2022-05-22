b66591b2dd6594246ab4c6820b5a9b03544795ed
Yasin Silavi <ys.silavi@gmail.com>
2022-05-22 16:55:48 +0430

    feat: add result view as lazy route

--- src/config/router.js
+++ src/config/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 // Views
 import HomeView from '/src/views/HomeView/index.vue';
 const QuizView = import('/src/views/QuizView/index.vue');
+const ResultView = import('/src/views/ResultView/index.vue');
 
 const routes = [
   {
@@ -12,6 +13,10 @@ const routes = [
   {
     path: '/quiz',
     component: QuizView,
+  },
+  {
+    path: '/result',
+    component: ResultView,
   },
 ];
 
