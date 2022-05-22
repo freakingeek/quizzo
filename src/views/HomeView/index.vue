af0a881e8ee7fc3ae6fe07c8f270f79609cda3d2
Yasin Silavi <ys.silavi@gmail.com>
2022-05-22 18:07:52 +0430

    feat: add leaderboard empty state

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -8,7 +8,7 @@
     <section id="leaderboard" class="leaderboard home-view__leaderboard">
       <h2 class="leaderboard__title">جدول امتیازات</h2>
 
-      <div class="leaders leaderboard__leaders">
+      <div v-if="leaderboard.length" class="leaders leaderboard__leaders">
         <template v-for="(leader, index) in leaderboard" :key="index">
           <div class="leader">
             <div class="leader__image-box">
@@ -19,6 +19,10 @@
             <span class="leader__score">{{ leader.score }} امتیاز</span>
           </div>
         </template>
+      </div>
+
+      <div class="leaderboard__empty">
+        <span >هنوز هیچ امتیازی ثبت نشده است ...</span>
       </div>
     </section>
   </DefaultLayout>
