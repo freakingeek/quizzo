9683315ef0efad3e234f363aeee6e4940753f3df
Yasin Silavi <ys.silavi@gmail.com>
2022-05-22 15:01:08 +0430

    feat: add quiz route as lazy route

--- src/config/router.js
+++ src/config/router.js
@@ -2,11 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 // Views
 import HomeView from '/src/views/HomeView/index.vue';
+const QuizView = import('/src/views/QuizView/index.vue');
 
 const routes = [
   {
     path: '/',
     component: HomeView,
+  },
+  {
+    path: '/quiz',
+    component: QuizView,
   },
 ];
 
