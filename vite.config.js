55064a28ea293467b8ba77fa4b64a624db38bc1d
Yasin Silavi <ys.silavi@gmail.com>
2023-01-16 23:35:26 +0330

    chore: set `@` as `src/` path resolver

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/quizzo/',
+
   plugins: [vue()],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
