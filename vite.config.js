77e2a803ded70a9d763b2d70d310f64e2387f5c6
Yasin Silavi <ys.silavi@gmail.com>
2023-04-12 01:27:37 +0330

    chore: config  pwa and manifest object

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,35 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/quizzo/',
 
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions: { enabled: true },
+      workbox: { globPatterns: ['**/*'] },
+      includeAssets: ['**/*'],
+      manifest: {
+        id: '/quizzo/',
+        name: 'Quizzo',
+        short_name: 'Quizzo',
+        description: 'The ultimate quiz app for entertainment!',
+        theme_color: '#e5383b',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
